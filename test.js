a33b0105fec6edc35db983d98d4f60668e69811e
driversline <n526258360@gmail.com>
2025-02-28 19:51:46 +0300

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const heading = document.createElement('h1');
-    heading.textContent = 'Привет!';
-    heading.style.color = '#c9d1d9';
-    heading.style.textAlign = 'center';
-    document.body.appendChild(heading);
-});
