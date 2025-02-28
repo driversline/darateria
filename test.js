0eba2e65e8d82e7b3e9bb7f17e6aaf684613436d
driversline <n526258360@gmail.com>
2025-02-28 19:39:26 +0300

    test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const heading = document.createElement('h1');
+    heading.textContent = 'Привет!';
+    heading.style.color = '#c9d1d9';
+    heading.style.textAlign = 'center';
+    document.body.appendChild(heading);
+});
